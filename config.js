91c16117f769ba17d3dc373f508f8101f290bf71
RenzGntng <renzzxkull@gmail.com>
2022-11-03 17:19:42 +0700

    walzjb

---
+++ config.js
@@ -0,0 +1,55 @@
+/**
+   * Create By Dika.
+   * Recode By Dimas Developer
+   * Contact Me on https://wa.me/6289522590235
+ 
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '0e92565522',
+}
+
+// Other
+global.owner = ['62881026434930']
+global.ownernomer = "62881026434930"
+global.premium = ['62881026434930']
+global.packname = 'Sticker Buatan'
+global.author = '▶ Dimas Ferdy\n▶ Owner : +6289522590235'
+global.sessionName = 'dimas-sesi'
+global.prefa = ['','!','.','#','/']
+global.sp = ''
+global.mess = {
+    success: 'Done',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/naze.png')
+global.vaze = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
